3a66d169e1c8d91d156864d65bfbd89f614a190a
Anne <anne@pay.nl>
2025-09-04 11:10:39 +0200

    Seperate Sofort (by Klarna)

---
+++ app/code/community/Pay/Payment/Model/Source/Paymentmethod/Sofortbyklarna/Active.php
@@ -0,0 +1,4 @@
+<?php
+class Pay_Payment_Model_Source_Paymentmethod_Sofortbyklarna_Active extends Pay_Payment_Model_Source_Paymentmethod_Active{
+     protected $_option_id = Pay_Payment_Model_Paymentmethod_Sofortbyklarna::OPTION_ID;
+}
