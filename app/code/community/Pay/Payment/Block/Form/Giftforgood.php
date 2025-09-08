3b8eb710db147a2946ed5d28d8f157f0825f07d5
Anne <anne@pay.nl>
2025-09-08 16:21:12 +0200

    Add Brite and Gift for Good

--- app/code/community/Pay/Payment/Block/Form/Giftforgood.php
+++ app/code/community/Pay/Payment/Block/Form/Giftforgood.php
@@ -4,6 +4,6 @@ class Pay_Payment_Block_Form_Giftforgood extends Pay_Payment_Block_Form_Abstract
 
     protected $paymentMethodId = Pay_Payment_Model_Paymentmethod_Giftforgood::OPTION_ID;
     protected $paymentMethodName = 'Gift for Good';
-    protected $methodCode = 'pay_payment_giftforgood';
+    protected $methodCode = 'pay_payment_Giftforgood';
     protected $template = 'pay/payment/form/default.phtml';
 }
