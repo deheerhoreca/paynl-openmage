cccfc09eb0a27d232e0d708bf97653abc2817edb
Anne <anne@pay.nl>
2025-09-08 16:29:49 +0200

    Add Pay by Bank

---
+++ app/code/community/Pay/Payment/Model/Paymentmethod/Paybybank.php
@@ -0,0 +1,8 @@
+<?php
+class Pay_Payment_Model_Paymentmethod_Paybybank extends Pay_Payment_Model_Paymentmethod {
+    const OPTION_ID = 2970;
+    protected $_paymentOptionId = 2970;
+    protected $_code = 'pay_payment_paybybank';
+    protected $_formBlockType = 'pay_payment/form_paybybank';
+}
+    
